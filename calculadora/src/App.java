79eb67792bcec78ac9a9e4f3c9c9339b174c356e
Jonathan Buitrago Roncancio <jonathanbuitragoroncancio50@gmail.com>
2022-08-29 08:27:18 -0500

    Add addition operation into facade.

--- calculadora/src/App.java
+++ calculadora/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         CalculatorFacade myCalculator = new CalculatorFacade(3, 2);
-        // System.out.println(myCalculator.additionOperation());
+        System.out.println(myCalculator.additionOperation());
         System.out.println(myCalculator.substractionOperation());
         System.out.println(myCalculator.multiplicationOperation());
     }
